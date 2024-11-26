4339bee41486f1760c8cdbc768f751b1ccaf0445
martines3000 <domajnko.martin@gmail.com>
2024-11-26 12:54:21 +0100

    chore: use old simple script

--- scripts/tests/nitro-testnode.sh
+++ scripts/tests/nitro-testnode.sh
@@ -5,16 +5,12 @@ cd "$MYDIR" || exit
 TEST_NODE_DIR="$MYDIR/../../nitro-testnode"
 cd ../..
 
-# Clone nitro
-export NITRO_SRC="$MYDIR/../../nitro"
-git clone --recurse-submodules git@github.com:OffchainLabs/nitro.git
-
 # Clone nitro-testnode
 git clone -b release --recurse-submodules https://github.com/OffchainLabs/nitro-testnode.git
 cd ./nitro-testnode || exit
 
 # Initialize test node
-./test-node.bash --no-run --init --dev || exit
+./test-node.bash --no-run --init || exit
 
 # Start with detached mode
-./test-node.bash --detach --dev
+./test-node.bash --detach
